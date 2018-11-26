ef9709592920bf736fb67288a8ca88aae3339ceb
Thomas Kendrick <tommkendrick@gmail.com>
2018-11-26 15:46:59 +0000

    Added tests for setters and getters

--- spec/students_spec.rb
+++ spec/students_spec.rb
@@ -12,4 +12,16 @@ class TestStudents < MiniTest::Test
     student = Students.new('Jimmy','E28')
     assert_equal('E28',student.cohort)
   end
+
+  def test_change_name
+    student = Students.new('Jimmy','E28')
+    student.name='Bob'
+    assert_equal('Bob', student.name)
+  end
+
+  def test_change_cohort
+    student = Students.new('Jimmy','E28')
+    student.cohort='E29'
+    assert_equal('E29', student.cohort)
+  end
 end
