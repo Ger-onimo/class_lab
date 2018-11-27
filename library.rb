0b835f9fb78dcbbe0d47185d2f8c17069a7b16f4
Gerry Waterston <gwaterston@hotmail.co.uk>
2018-11-27 01:13:25 +0000

    half of extension

--- library.rb
+++ library.rb
@@ -7,5 +7,17 @@ attr_reader :books      #passed
     @books = books
   end
 
+  # def book_info(books) # not passed
+  #   book_info_collect = []
+  #     for titles in books
+  #       book_info_collect.push(titles)
+  #     end
+  # end
 
+  def rent_details(books) #passed
+    details_collect = []
+      for rentals in books[:rental_details]
+        details_collect.push(rentals)
+      end
+   end
 end
